a859d0d27668b991c421d52315bdbb1562dd0ab8
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-12-04 13:43:12 +0900

    published

---
+++ js/replaceAll.js
@@ -0,0 +1,35 @@
+/**
+ * @author JungHyunKwon
+ * @version 1.0.0
+ */
+try {
+	(function() {
+		'use strict';
+
+		/**
+		 * @name replaceAll
+		 * @param {string} value
+		 * @param {string} from
+		 * @param {string} to
+		 * @return {string}
+		 * @since 2018-07-13
+		 */
+		window.replaceAll = function(value, from, to) {
+			var result = '';
+			
+			//문자일 때
+			if(typeof value === 'string') {
+				result = value;
+				
+				//문자일 때
+				if(typeof from === 'string' && typeof to === 'string') {
+					result = result.replace(new RegExp(from, 'gm'), to);
+				}
+			}
+
+			return result;
+		};
+	})();
+}catch(e) {
+	console.error(e);
+}
