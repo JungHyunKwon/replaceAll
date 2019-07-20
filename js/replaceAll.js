3ae454bd571338edf1d9edb3e3129aa11daa8df7
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:14:43 +0900

    published

--- js/replaceAll.js
+++ js/replaceAll.js
@@ -2,46 +2,42 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function() {
-		'use strict';
+(function() {
+	'use strict';
 
-		var _toString = Object.prototype.toString;
+	var _toString = Object.prototype.toString;
 
-		/**
-		 * @name 정규식 확인
-		 * @since 2017-12-06
-		 * @param {*} value
-		 * @return {boolean}
-		 */
-		function _isRegExp(value) {
-			return _toString.call(value) === '[object RegExp]';
-		}
+	/**
+	 * @name 정규식 확인
+	 * @since 2017-12-06
+	 * @param {*} value
+	 * @return {boolean}
+	 */
+	function _isRegExp(value) {
+		return _toString.call(value) === '[object RegExp]';
+	}
 
-		/**
-		 * @name replaceAll
-		 * @param {string} value
-		 * @param {string || regexp} from
-		 * @param {string} to
-		 * @return {string}
-		 * @since 2018-07-13
-		 */
-		window.replaceAll = function(value, from, to) {
-			var result = '';
+	/**
+	 * @name replaceAll
+	 * @param {string} value
+	 * @param {string || regexp} from
+	 * @param {string} to
+	 * @return {string}
+	 * @since 2018-07-13
+	 */
+	window.replaceAll = function(value, from, to) {
+		var result = '';
+		
+		//문자일 때
+		if(typeof value === 'string') {
+			result = value;
 			
 			//문자일 때
-			if(typeof value === 'string') {
-				result = value;
-				
-				//문자일 때
-				if(typeof to === 'string' && typeof from === 'string' || _isRegExp(from)) {
-					result = result.replace(new RegExp(from, 'g'), to);
-				}
+			if((typeof from === 'string' || _isRegExp(from)) && typeof to === 'string') {
+				result = result.replace(new RegExp(from, 'g'), to);
 			}
+		}
 
-			return result;
-		};
-	})();
-}catch(e) {
-	console.error(e);
-}
+		return result;
+	};
+})();
